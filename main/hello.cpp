2be5eeba02e212d007c745957b8b3e9be495adfa
vandana <80338312+vandana265@users.noreply.github.com>
2023-02-19 22:54:11 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "My SRN is PES1UG20CS247\n";
+  cout << "My name is Manvanth Gs\n";
+  return 0;
+}
