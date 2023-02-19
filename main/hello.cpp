e51442a7a6628ae1a735afb6c2f9c55c53739427
vandana <80338312+vandana265@users.noreply.github.com>
2023-02-19 22:54:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "My SRN is PES1UG20CS247\n";
-  cout << "My name is Manvanth Gs\n";
+  cout << "My SRN is PES1UG20CS482\n";
   return 0;
 }
